8ad9fe8bc9ffd7ada2cf75c35f6d20a45a95c6ac
Tom Clarkson <tom@designsight.co.nz>
2021-12-10 11:42:04 +1300

    update slider

---
+++ slider/Colors.tsx
@@ -0,0 +1,66 @@
+/** @format */
+
+class Color {
+  /* These are directly invertable */
+  /* These are in order from light to dark - they are greyscale from Maya */
+  readonly white: string = "#FFFFFF";
+
+  readonly background: string = "#FBFBFC";
+
+  readonly rocks: string = "#EAEDF2";
+
+  readonly moonshine: string = "#DADCE0";
+
+  readonly cloudyBlue: string = "#BCC1C5";
+
+  readonly battleship: string = "#5F6368";
+
+  readonly shark: string = "#25272B";
+
+  readonly black: string = "#000000";
+
+  /* Other greyscale colors */
+  readonly snow: string = "#FBFBFD";
+
+  readonly primary: string = "#fffaed";
+
+  readonly dusk: string = "#BCC1C5";
+
+  readonly disabled: string = "#8e959e";
+
+  /* Special Colours */
+  readonly accent: string = "#251E5B";
+
+  readonly body: string = "#EC6C57";
+
+  readonly celtic: string = "#23E095";
+
+  readonly leprechaun: string = "#24B379";
+
+  readonly chilling: string = "#DE6EA1";
+
+  readonly delete: string = "#FF564F";
+
+  readonly goodyBag: string = "#FFB633";
+
+  readonly littleMissNoName: string = "#6E71EC";
+
+  readonly manUtd: string = "#ff564f";
+
+  readonly mind: string = "#6689DB";
+
+  readonly night: string = "#100b57";
+
+  readonly secondary: string = "#251E5B";
+
+  readonly skyblue: string = "#4d7aff";
+
+  readonly sleep: string = "#9660D7";
+
+  readonly spirit: string = "#7ED3B1";
+
+  readonly transparent: string = "#00000000";
+}
+
+const Colors = new Color();
+export default Colors;
--- slider/Slider.tsx
+++ slider/Slider.tsx
@@ -5,15 +5,23 @@ import Animated, {
   useSharedValue,
   useAnimatedGestureHandler,
   useAnimatedStyle,
+  interpolateNode,
+  Extrapolate,
+  interpolateColors,
 } from "react-native-reanimated";
 import {
   PanGestureHandler,
   PanGestureHandlerGestureEvent,
 } from "react-native-gesture-handler";
+import { clamp } from "./utils";
 
 const SLIDER_WIDTH = 300;
 const KNOB_WIDTH = 70;
 const MAX_RANGE = 20;
+
+const SLIDER_RANGE = SLIDER_WIDTH - KNOB_WIDTH;
+
+const BALLOON_WIDTH = 60;
 
 export const Slider = () => {
   const translateX = useSharedValue(0);
@@ -28,7 +36,11 @@ export const Slider = () => {
     },
     onActive: (event, ctx) => {
       isSliding.value = true;
-      translateX.value = event.translationX + ctx.offsetX;
+      translateX.value = clamp(
+        event.translationX + ctx.offsetX,
+        0,
+        SLIDER_WIDTH - KNOB_WIDTH
+      );
     },
     onEnd: () => {
       isSliding.value = false;
@@ -39,19 +51,66 @@ export const Slider = () => {
     return { transform: [{ translateX: translateX.value }] };
   });
 
+  const balloonTranslationStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value + (KNOB_WIDTH - BALLOON_WIDTH) / 2 },
+      ],
+    };
+  });
+
   const progressStyle = useAnimatedStyle(() => {
     return {
       width: translateX.value + KNOB_WIDTH,
     };
   });
 
+  // const rotateStyle = useAnimatedStyle(() => {
+  //   const rotate = interpolateNode(translateX.value, {
+  //     inputRange: [0, SLIDER_RANGE], // between the beginning and end of the slider
+  //     outputRange: [0, 4 * 360], // penguin will make 4 full spins
+  //     extrapolate: Extrapolate.CLAMP,
+  //   });
+
+  //   return {
+  //     transform: [{ rotate: `${rotate}deg` }],
+  //   };
+  // });
+
+  //   const ballonBackgroundColor = interpolateColors(percentAnimatedValue.current, {
+  //     inputRange: [0, 0.5, 1],
+  //     outputColorRange: [Colors.delete, Colors.littleMissNoName, Colors.celtic],
+  // });
+
+  console.log({ scrollTranslationStyle });
+
   return (
-    <View style={styles.slider}>
-      <Animated.View style={[styles.progress, progressStyle]} />
-      <PanGestureHandler onGestureEvent={onGestureEvent}>
-        <Animated.View style={[styles.knob, scrollTranslationStyle]} />
-      </PanGestureHandler>
-    </View>
+    <>
+      <View
+        style={{
+          width: SLIDER_WIDTH,
+          marginBottom: 30,
+        }}
+      >
+        <Animated.View
+          testID="Balloon"
+          style={[
+            {
+              height: 80,
+              width: BALLOON_WIDTH,
+              backgroundColor: "pink",
+            },
+            balloonTranslationStyle,
+          ]}
+        />
+      </View>
+      <View style={styles.slider}>
+        <Animated.View style={[styles.progress, progressStyle]} />
+        <PanGestureHandler onGestureEvent={onGestureEvent}>
+          <Animated.View style={[styles.knob, scrollTranslationStyle]} />
+        </PanGestureHandler>
+      </View>
+    </>
   );
 };
 
