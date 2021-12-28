2efe3710be37a4682f5e21963913e36759539e48
Tom Clarkson <tom@designsight.co.nz>
2021-12-28 14:08:55 +1300

    add navigation

--- App.tsx
+++ App.tsx
@@ -1,15 +1,56 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 // import AnimatedStyleUpdateExample from "./AnimatedStyleUpdateExample";
 import { Slider } from "./slider/Slider";
 // import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: { userId: string };
+};
+
+function HomeScreen({
+  navigation,
+}: NativeStackScreenProps<RootStackParamList, "Home">) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details", { userId: "1" })}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Slider />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+
+  // return (
+  //   <View style={styles.container}>
+  //     <Slider />
+  //   </View>
+  // );
   // return <AnimatedStyleUpdateExample />;
   // return (
   //   <View style={styles.container}>
